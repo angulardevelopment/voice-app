04065b2e7728cff4aa4b0e949be76b3fbab5148a
DESKTOP-1ASQ7K6\sunny <sunny.gupta@docquity.com>
2021-05-12 21:09:30 +0530

    Speech Recognition system added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # About
 
 Text to speech app
-
+Speech Recognition system added
 # Voice
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.0.3.
--- src/app/voice/voice.component.ts
+++ src/app/voice/voice.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Location } from '@angular/common';
-
+export interface IWindow extends Window {
+  webkitSpeechRecognition: any;
+  SpeechRecognition: any;
+  webkitSpeechGrammarList: any;
+  webkitSpeechRecognitionEvent: any;
+}
 @Component({
   selector: 'app-voice',
   templateUrl: './voice.component.html',
@@ -57,6 +62,59 @@ export class VoiceComponent implements OnInit {
     toggle;
 });
   stopButton.addEventListener('click', () => toggle(false));
+  }
+
+// another component
+
+  speechSystem() {
+      const webkitSpeechRecognition = window['webkitSpeechRecognition'];
+      // const { webkitSpeechRecognition }: IWindow = this.window;
+
+      // const { webkitSpeechGrammarList }: IWindow = <IWindow>window;
+      // const { webkitSpeechRecognitionEvent }: IWindow = <IWindow>window;
+
+      var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+      // var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+      // var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+      // window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+      const recognition = new SpeechRecognition();
+      recognition.interimResults = true;
+      recognition.lang = 'en-US';
+      recognition.maxAlternatives = 1;  //Sets the number of alternative potential matches that should be returned per result
+      // SpeechRecognition.continuous =true;
+      let p = document.createElement('p');
+      const words = document.querySelector('.words');
+      words.appendChild(p);
+
+      //Receiving and handling results
+      // recognition.onresult = function(event) {
+      //   var last = event.results.length - 1;
+      //   var color = event.results[last][0].transcript;
+      //   diagnostic.textContent = 'Result received: ' + color + '.';
+      //   bg.style.backgroundColor = color;
+      //   console.log('Confidence: ' + event.results[0][0].confidence);
+      // }
+
+      recognition.addEventListener('result', e => {
+        console.log(e.results, 'f');
+
+        const transcript = Array.from(e.results)
+          .map(result => result[0])
+          .map(result => result.transcript)
+          .join('');
+
+        const poopScript = transcript.replace(/poop|poo|shit|dump/gi, '💩');
+        p.textContent = poopScript;
+
+        if (e.results[0].isFinal) {
+          p = document.createElement('p');
+          words.appendChild(p);
+        }
+      });
+
+      recognition.addEventListener('end', recognition.start);
+      recognition.start();  //'Ready to receive a  command
   }
 
 }
